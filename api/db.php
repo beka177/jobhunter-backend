a8e75362f388fcc1c42546a3ae0e79a5044b2131
beka177 <bekaseitkali@gmail.com>
2026-05-25 19:40:16 +0500

    fix: allow PATCH and PUT in CORS headers

--- api/db.php
+++ api/db.php
@@ -1,7 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $host = 'localhost';
 $db   = 'jobsearch';
